42e6914daeae1474945fb93f37e2d9a0e489f95a
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-04-17 17:57:38 -0600

    Still having trouble, wrote out DarkMode code

--- client/src/components/UseLocalStorage.js
+++ client/src/components/UseLocalStorage.js
@@ -1,10 +1,10 @@
 import {useState} from 'react'
 
-export const useLocalStorage = (keuy, initialValue) => {
+export const useLocalStorage = (key, initialValue) => {
     const item = window.localStorage.getItem(key);
     const setValue = value => {
         setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value))]
+        window.localStorage.setItem(key, JSON.stringify(value))
     }
     const [storedValue, setStoredValue] = useState(() => {
         return item ? JSON.parse(item) : initialValue;
